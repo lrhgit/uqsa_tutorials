bff1003f30bb996eae1cde65cfc667bac851ac8d
lrhgit <leif.r.hellevik@ntnu.no>
2017-06-24 12:07:55 +0200

    updates

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -x # display shell commands prior to execution
+
+
+path=~/git/KT8302_notes/tutorials
+
+# copy movies that follow mov-ch?-convention
+for nbook in $( ls -d *.ipynb ); do
+    cp $path/$nbook .
+done
+
+
+
+
+
